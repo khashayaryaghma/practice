226d06cb6fc9be86a4c0ef9433eb58a7de6e5371
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-09 14:20:52 +0430

    Update index.js

--- Drag&Drop/S01/index.js
+++ Drag&Drop/S01/index.js
@@ -7,10 +7,14 @@ fill.addEventListener("dragend", dragEnd);
 
 // Drag Functions
 function dragStart() {
-    this.classList.add("hold");
-    setTimeout(()=>{this.classList.add("invisible");}, 0)
+  this.classList.add("hold");
+  setTimeout(() => {
+    this.classList.add("invisible");
+  }, 0);
 }
 
 function dragEnd() {
-    
+  this.classList.remove("invisible");
+  this.classList.remove("hold");
+  this.classList.add("fill");
 }
