31bf15b3ce69de7a0e1d94106f7ff8d9c6a8569d
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-09 22:18:06 +0430

    Update index.js

--- Drag&Drop/S02/index.js
+++ Drag&Drop/S02/index.js
@@ -23,11 +23,15 @@ createList();
 
 // Insert list items into DOM
 function createlist() {
-    [...richestPeople].map((person, index)=>{
-        const listItem =document.createElement("li")
+  [...richestPeople].map((person, index) => {
+    const listItem = document.createElement("li");
 
-        listItem.setAttribute("data-index", index)
+    listItem.setAttribute("data-index", index);
 
-        listItem.innerHTML = `<span class = "number">${index + 1}</span> <div class="dragable" draggable="true"><p class="person-name">${person}</p></div> `
-    })
+    listItem.innerHTML = `<span class = "number">${
+      index + 1
+    }</span> <div class="dragable" draggable="true"><p class="person-name">${person}</p> <i class="fas fa-grip-lines"></i></div> `;
+
+    listItems.push(listItem)
+  });
 }
