e9ffc98afe39069657dce5ce26fbfbe82f26a2e8
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-10 20:18:28 +0430

    Update index.js

--- Cookies-LocalStorage-Session/index.js
+++ Cookies-LocalStorage-Session/index.js
@@ -3,3 +3,6 @@ localStorage.setItem("name", "khashayar")
 
 sessionStorage.setItem("name", "hadi")
 console.log(sessionStorage.getItem("name"));
+
+document.cookie = "name-sohrab; expires=" + new Date(2022, 6, 7).toUTCString();
+document.cookie = "newName-rostam; expires=" + new Date(3654, 3, 1).toUTCString();
