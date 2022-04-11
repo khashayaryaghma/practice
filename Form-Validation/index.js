0dc7ec2d3ff7ecce09f473485209bb67e44c390c
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-11 13:25:35 +0430

    Update index.js

--- Form-Validation/index.js
+++ Form-Validation/index.js
@@ -39,16 +39,16 @@ function validatePhone() {
   return true;
 }
 
-function validateEmail(){
-    let email = document.getElementById("contact-email").value;
+function validateEmail() {
+  let email = document.getElementById("contact-email").value;
 
-    if (email.length === 0) {
-        emailError.innerHTML = "email is required";
-    }
-    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
-        emailError.innerHTML = "Email Invalid"
-        return false;
-    }
-    emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
-    return true;
+  if (email.length === 0) {
+    emailError.innerHTML = "email is required";
+  }
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = "Email Invalid";
+    return false;
+  }
+  emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
+  return true;
 }
