c750e6345bf6d8cced72fb24b47260787b2249e2
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-11 12:20:33 +0430

    Update index.js

--- Form-Validation/index.js
+++ Form-Validation/index.js
@@ -1,13 +1,20 @@
-const nameError = document.getElementById("name-error")
+const nameError = document.getElementById("name-error");
 const phoneError = document.getElementById("phone-error");
 const emailError = document.getElementById("email-error");
 const messageError = document.getElementById("message-error");
 const submitError = document.getElementById("submit-error");
 
-function validateName(){
-    let name = document.getElementById("contact-name").value;
+function validateName() {
+  let name = document.getElementById("contact-name").value;
 
-    if (name.length === 0) {
-        nameError.innerHTML = "name is required";
-    }
+  if (name.length === 0) {
+    nameError.innerHTML = "name is required";
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
+    nameError.innerHTML = "Write full name";
+    return false;
+  }
+  nameError.innerHTML = "valid";
+  return true;
 }
