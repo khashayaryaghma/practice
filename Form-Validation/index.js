24fe689668ac9548489da303084c351851afb36e
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-11 12:27:45 +0430

    Update index.js

--- Form-Validation/index.js
+++ Form-Validation/index.js
@@ -15,6 +15,6 @@ function validateName() {
     nameError.innerHTML = "Write full name";
     return false;
   }
-  nameError.innerHTML = "valid";
+  nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
   return true;
 }
