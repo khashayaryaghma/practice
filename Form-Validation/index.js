adfa8eea6e4adb3362a6583ebd7425f516ab7a97
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-11 12:16:22 +0430

    Update index.js

--- Form-Validation/index.js
+++ Form-Validation/index.js
@@ -3,3 +3,11 @@ const phoneError = document.getElementById("phone-error");
 const emailError = document.getElementById("email-error");
 const messageError = document.getElementById("message-error");
 const submitError = document.getElementById("submit-error");
+
+function validateName(){
+    let name = document.getElementById("contact-name").value;
+
+    if (name.length === 0) {
+        nameError.innerHTML = "name is required";
+    }
+}
