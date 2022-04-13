ec3f94a747e4bc9e72c7b6babbd17f088c815c50
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-13 22:43:48 +0430

    Update details.js

--- JSON-Server/js/details.js
+++ JSON-Server/js/details.js
@@ -1,5 +1,19 @@
 const id = new URLSearchParams(window.location.search).get("id");
 
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id);
+  if (!res.ok) {
+    window.location.replace("/");
+  }
+  const post = await res.json();
+
+  const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+  `;
+
+  container.innerHTML = template;
+};
 
 
 
