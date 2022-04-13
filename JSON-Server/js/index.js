803c4f0929c084d75e7e650b940c48f1b2025dc5
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-13 21:54:16 +0430

    Update index.js

--- JSON-Server/js/index.js
+++ JSON-Server/js/index.js
@@ -1,1 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => renderPosts());
+
+const renderPosts = async (term) => {
+  console.log(term);
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
+
+  let template = '';
+  posts.forEach(post => {
+    template += `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0, 200)}...</p>
+        <a href="/details.html?id=${post.id}">Read more</a>
+      </div>
+    `
+  });
