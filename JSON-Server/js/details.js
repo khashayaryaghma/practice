e959a0bbba73e9c55ca711cecafddbfda6b73f18
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-13 23:28:20 +0430

    Update details.js

--- JSON-Server/js/details.js
+++ JSON-Server/js/details.js
@@ -1,5 +1,6 @@
 const id = new URLSearchParams(window.location.search).get("id");
 const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
 
 const renderDetails = async () => {
   const res = await fetch("http://localhost:3000/posts/" + id);
@@ -16,7 +17,11 @@ const renderDetails = async () => {
   container.innerHTML = template;
 };
 
-
-
+deleteBtn.addEventListener("click", async () => {
+  const res = await fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+  });
+  window.location.replace("/");
+});
 
 window.addEventListener("DOMContentLoaded", renderDetails);
