64ebcb84be6756e5b15146b5d08a903207fac2d7
khashayaryaghma <92808021+khashayaryaghma@users.noreply.github.com>
2022-04-13 23:21:35 +0430

    Update details.js

--- JSON-Server/js/details.js
+++ JSON-Server/js/details.js
@@ -1,4 +1,5 @@
 const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
 
 const renderDetails = async () => {
   const res = await fetch("http://localhost:3000/posts/" + id);
